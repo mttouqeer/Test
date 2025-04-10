70606f2f445f61e28990ec2af6f90bac86eadab0
Touqeer <touqeeroff@gmail.com>
2025-04-10 09:31:12 +0500

    Initial Commit

---
+++ Test/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by MacBook Pro on 30/01/2025.
+//
+
+import UIKit
+import UIView_Shimmer
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.setTemplateWithSubviews(true,color: .lightGray)
+    }
+    
+}
+
+
+extension UILabel: ShimmeringViewProtocol { }
+extension UIButton: ShimmeringViewProtocol { }
+extension UITextView: ShimmeringViewProtocol { }
